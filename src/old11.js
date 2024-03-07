64fc186a0793b34929d27439028cdbe6ad7427a4
Your Name <you@example.com>
2024-03-07 11:48:36 +0000

    new2 sd ok

---
+++ src/old11.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  const images = [
+    { name: 'image1.jpg', url: './images/image1.jpg' },
+    // Add more images here
+  ];
+
+  const imageList = images.map(image => (
+    <img key={image.name} src={image.url} alt={image.name} />
+  ));
+
+  return (
+    <div>
+      <h1>Welcome to the Black Tea Swap...</h1>
+      {imageList}
+    </div>
+  );
+}
+
+export default App;
+
